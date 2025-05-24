ee027bf2e5ef264bbe282fdc8baac7dd223f45a8
AVELAR\yesse <avelaryesser@gmail.com>
2025-05-24 16:58:09 -0600

    Arreglando rutas principales de navegacion

--- src/App.js
+++ src/App.js
@@ -2,32 +2,26 @@ import React from "react";
 import Inicio from "./Inicio";
 import AgregarEvento from "./AgregarEvento";
 import Error404 from "./Error";
-
-import { Routes, Route } from "react-router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sobre from "./SobreNosotros";
 
 function App() {
-  const mostrarError = true;
-
   return (
-    <div>
-      <nav class="navbar navbar-light bg-light"> 
-        <a className="navbar-brand" href="/">Eventify</a> 
+    <Router>
+      <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="/">Eventify</a>
         <ul>
           <li><a href="/agregar">Agregar</a></li>
           <li><a href="/sobre">Sobre</a></li>
         </ul>
       </nav>
-
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/agregar" element={<AgregarEvento />} />
         <Route path="/sobre" element={<Sobre />} />
-        <Route path="/error" element={<Error404 />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
-
-      {<Error404 />}
-    </div>
+    </Router>
   );
 }
 
